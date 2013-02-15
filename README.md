9a36eea11ca6beba805bcb2389c3b7c3ec361ea5
Max.Uhlenhuth <max@silviaterra.com>
2013-02-15 00:16:12 -0500

    big restructuring and cleanup

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 offline_map_poc
 ===============
+Proof of concept for offline mobile mapping with PhoneGap, Leaflet, and MapBox.
 
-Proof of concept for offline mobile mapping with PhoneGap, Leaflet, and MapBox
+Demo Android app available for download [here](https://build.phonegap.com/apps/311874/share)
 
-Gotchas:
- * Domain whitelisting in config - (at least mapbox)
+##Usage
+Simply enter a MapBox map id (looks like __example.map-asd23rds___) or several
+(separated by a comma) and click the "Download Tiles" button.  You can click
+"Clear Tiles" to clear the tiles.
+
+##Code Architecture
+ * tileUtils.js - functions for getting tile URLs for an area
+ * fileUtils.js - functions for downloading a bunch of files to the local
+ filesystem (and displaying a nice modal progress bar)
+ * main.js - some UI and bookkeeping code
+
+##Extending this code
+This project is solely intended to be a proof of concept.  The guts  
+
+##Gotchas
+ * Domain whitelisting in config.xml - make sure you can access MapBox servers
--- main.js
+++ main.js
@@ -1,26 +1,4 @@
-//sloppy globals but ok for now
-var MAP;
-var MAP_CONTROL;
-var BASE_LAYERS = {};
 var FILESYSTEM;
-
-function clearTiles(callback) {
-    FILESYSTEM.root.getDirectory('tiles', {create: true},
-        function(dir) { //success
-           dir.removeRecursively(
-                function() { callback(); }, 
-                function(){ alert("Error deleting!"); }
-            );
-        }, 
-        function() { alert("Error deleting tiles")} //fail
-    );
-}
-
-function tileFormat4FS(fileSystem, mapID) {
-    var rootDir = fileSystem.root.fullPath;
-    if (rootDir[rootDir.length-1] != '/') { rootDir += '/'; }
-    return rootDir + 'tiles/' + mapID + '/{z}/{x}/{y}.png';
-}
 
 function getMapIDs() {
     /*
@@ -31,50 +9,12 @@ function getMapIDs() {
     else { return mapboxIDs; }
 }
 
-function reloadMap(options) {
-    /*
-     * Clear layers and reset based
-     * on current mapIDs
-     */
-     
-    //handle options
-    var clear = options['clear'] || false;
-
-    if (!MAP) { //initialize map if first time
-        MAP = L.map('map', {'minZoom': 3, 'maxZoom': 17}).setView([38.255, -85.73], 15);
-    }
-    
-    //clear out old MAP_CONTROL
-    if (MAP_CONTROL) { MAP.removeControl(MAP_CONTROL); MAP_CONTROL=null; }
-
-    //clear out old layers
-    for (mapID in BASE_LAYERS) {
-        var lyr = BASE_LAYERS[mapID];
-        lyr.redraw(); //clear tiles
-        MAP.removeLayer(lyr);
-    }
-    BASE_LAYERS = {};
-    
-    if (clear) { return; } //job done if just clear
-    
-    var mapboxIDs = getMapIDs();
-    if (mapboxIDs == null) { return; } //no ids
-    
-    //add a layer for each mapbox ID
-    for (var i=0, l=mapboxIDs.length; i<l; i++) {
-        var mapID = mapboxIDs[i];
-        var format = tileFormat4FS(FILESYSTEM, mapID);
-        var lyr = L.tileLayer(format, {
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; MapBox',
-            minZoom: 3,
-            maxZoom: 17
-        })
-        BASE_LAYERS[mapID] = lyr;
-        lyr.addTo(MAP); 
-    }
-    
-    MAP_CONTROL = L.control.layers(BASE_LAYERS, {});
-    MAP_CONTROL.addTo(MAP);
+function createMapOptions(clear) {
+    return {
+        'clear': clear,
+        'fileSystem': FILESYSTEM,
+        'mapIDs': getMapIDs()
+    };
 }
 
 $(document).ready(function() {
@@ -91,23 +31,33 @@ $(document).ready(function() {
             LocalFileSystem.PERSISTENT, 0, 
             function(fs) { //success
                 FILESYSTEM = fs; //set global - sloppy, I know
-                reloadMap({'clear':false});
+                mapUtils.reloadMap(createMapOptions(false));
             },
-            function() { alert("Failure!"); } //filesystem failure
+            function() { alert("Failure accessing filesystem!"); } //filesystem failure
         );
         
         $("#clear").off("click").on("click", function() {
-            clearTiles(function(){ reloadMap({'clear':true}); alert("Tiles cleared successfully"); });
+            fileUtils.rmDir(
+                FILESYSTEM,
+                'tiles',
+                function(){ 
+                    mapUtils.reloadMap(createMapOptions(true)); 
+                    alert("Tiles cleared successfully"); 
+                }
+            );
         });
 
         $("#download").off("click").on("click", function() {
             var mapboxIDs = getMapIDs();
             if (mapboxIDs == null) { alert("Enter a MapBox Map ID"); return; } //no ids
-            bulkDownload(
-               pyramid(mapboxIDs, 38.255, -85.73, {'minZoom':14, 'maxZoom':16}), //tile urls
+            fileUtils.bulkDownload(
+               tileUtils.pyramid(mapboxIDs, 38.255, -85.73, {'minZoom':14, 'maxZoom':16}), //tile urls
                'tiles',
                $("#progress_modal"),
-               function() { alert("Download successful!"); reloadMap({'clear':false}); }
+               function() { 
+                   alert("Download successful!"); 
+                   mapUtils.reloadMap(createMapOptions(false)); 
+               }
             );
         });
     }); 
