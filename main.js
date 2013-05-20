2a2c2e1132dddf76a87baff2d0c9b76a1c8e9130
Syrus <syrus.mesdaghi@lmnsolutions.com>
2013-05-20 11:09:21 -0400

    pull radius of 10 so that we end up with about 20k tiles

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ $(document).ready(function() {
             var mapboxIDs = getMapIDs();
             if (mapboxIDs == null) { alert("Enter a MapBox Map ID"); return; } //no ids
             fileUtils.bulkDownload(
-               tileUtils.pyramid(mapboxIDs, 38.255, -85.73, {}), //tile urls
+								   tileUtils.pyramid(mapboxIDs, 38.255, -85.73, {radius: 10}), //tile urls
                'tiles',
                $("#progress_modal"),
                function() { 
